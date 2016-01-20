24c6c16a42386ffad48f96786eb8db1c582c54db
Mila Ruseva <msruseva@gmail.com>
2016-01-20 19:05:23 +0200

    Third solution refactor

--- solutions/03.rb
+++ solutions/03.rb
@@ -1,12 +1,18 @@
+class Integer
+
+  def is_prime?
+    self == 1 ? false : (2..self / 2).none?{ |i| self % i == 0 }
+  end
+
+end
+
 class RationalSequence
   include Enumerable
-
 
   def initialize(limit)
     @limit = limit
     @direction = :left
-    @nominator = 1
-    @denominator = 1
+    @nominator, @denominator = 1, 1
     @rational_numbers = []
   end
 
@@ -39,6 +45,7 @@ class RationalSequence
   def divide
     Rational(@nominator, @denominator)
   end
+
 end
 
 class PrimeSequence
@@ -48,48 +55,36 @@ class PrimeSequence
     @limit = limit
   end
 
-  def self.is_prime(number)
-    return false if number == 1
-    divider = 2
-    while number > divider do
-      if number % divider == 0
-        return false
-      end
-      divider += 1
-    end
-    return true
-  end
-
   def each
-    found_prime_numbers = 0
-    current = 2
+    found_prime_numbers, current = 0, 2
+
     while found_prime_numbers < @limit  do
-      if PrimeSequence.is_prime(current)
+      if current.is_prime?
         found_prime_numbers += 1
         yield current
       end
       current += 1
     end
   end
+
 end
 
 class FibonacciSequence
   include Enumerable
 
-  def initialize(limit, options = {})
+  def initialize(limit, first: 1, second: 1)
     @limit = limit
-    @first = options[:first] || 1
-    @second = options[:second] || 1
+    @first = first
+    @second = second
   end
 
   def each
     current, previous = @second, @first
     counter = 1
+
     while counter <= @limit
       yield previous
-      temp = previous
-      previous = current
-      current = current + temp
+      current, previous = current + previous, current
       counter += 1
     end
   end
@@ -99,39 +94,29 @@ module DrunkenMathematician
   module_function
 
   def meaningless(n)
-    rationals = RationalSequence.new(n)
-    group_one = []
-    group_two = []
-    rationals.each do |rat|
-      if PrimeSequence.is_prime(rat.numerator) or PrimeSequence.is_prime(rat.denominator)
-        group_one << rat
-      else
-        group_two << rat
-      end
+    rational = RationalSequence.new(n).partition do |r|
+      r.numerator.is_prime? or r.denominator.is_prime?
     end
-    group_one.reduce(1, :*) / group_two.reduce(1, :*)
+
+    rational.first.inject(1, :*) / rational.last.inject(1, :*)
   end
 
   def aimless(n)
-    primes = PrimeSequence.new(n)
     sum = 0
-    primes.each_slice(2) do |pair|
-      sum += Rational(pair[0], (pair[1] || 1))
+    PrimeSequence.new(n).each_slice(2) do |pair|
+      sum += Rational(pair[0], pair[1] || 1)
     end
     sum
   end
 
   def worthless(n)
-    seq = RationalSequence.new(2**16)
-    sum = 0
-    result = []
-    fibonacci_max = FibonacciSequence.new(n).max
-    seq.each do |rat|
-      sum += rat
-      if sum > fibonacci_max
-        return result
-      end
-      result << rat
+    sequence, sum = RationalSequence.new(2**16), 0
+    fibonacci_max = FibonacciSequence.new(n).to_a.last
+
+    sequence.take_while do |number|
+      sum += number
+      sum <= fibonacci_max
     end
+
   end
 end
