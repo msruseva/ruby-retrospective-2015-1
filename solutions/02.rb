a36ea565bb6937075b0a073f89585e8d51a96915
Mila Ruseva <msruseva@gmail.com>
2016-01-20 16:37:31 +0200

    Second solution refactor

--- solutions/02.rb
+++ solutions/02.rb
@@ -1,10 +1,48 @@
 def move(snake, direction)
-  snake_new = snake[1..-1]
-  grow(snake_new, direction)
+  grow(snake, direction).drop(1)
 end
 
 def grow(snake, direction)
-  snake_new = snake.dup
-  head = new_head(snake, direction)
-  snake_new.push(head)
+  snake + [new_head(snake_head(snake), direction)]
+end
+
+def snake_head(snake)
+  snake[-1]
+end
+
+def new_head(head, direction)
+  [head[0] + direction[0], head[1] + direction[1]]
+end
+
+def new_food(food, snake, dimensions)
+  position = new_position(dimensions)
+  while snake.include?(position) or food.include?(position) do
+    position = new_position(dimensions)
+  end
+  position
+end
+
+def new_position(dimensions)
+  xs = rand(dimensions[:width])
+  ys = rand(dimensions[:height])
+  [xs, ys]
+end
+
+def obstacle_ahead?(snake, direction, dimensions)
+  head = new_head(snake_head(snake), direction)
+  snake.include?(head) or wall?(head, dimensions)
+end
+
+def wall?(position, dimensions)
+  return true if position[0] < 0
+  return true if position[0] >= dimensions[:width]
+  return true if position[1] < 0
+  return true if position[1] >= dimensions[:height]
+  return false
+end
+
+def danger?(snake, direction, dimensions)
+  return true if obstacle_ahead?(snake, direction, dimensions)
+  next_turn = move(snake, direction)
+  obstacle_ahead?(next_turn, direction, dimensions)
 end
